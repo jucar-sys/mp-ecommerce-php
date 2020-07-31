580f2e3fa3c2e21746a80f7383933dd6ff14776c
Juan Carlos García <jucar.sys@gmail.com>
2020-07-31 09:53:54 -0500

    Update Config

--- includes/config.php
+++ includes/config.php
@@ -13,16 +13,17 @@
     define("COMP_ID", "535650015");
 
     // URL IMGS
-    define("URLIMG", "http://localhost/mp-ecommerce-php/");
+    define("URLIMG", "https://juancarlosgar-mp-ecommerce-php.herokuapp.com/");
+    // define("URLIMG", "http://localhost/mp-ecommerce-php/");
 
     
     // URL_REDIRECTS
-    // define("SUCCESS", "https://juancarlosgar-mp-ecommerce-php.herokuapp.com//verificador_pp.php?status=success");
-    // define("FAILURE", "https://juancarlosgar-mp-ecommerce-php.herokuapp.com//verificador_pp.php?status=failure");
-    // define("PENDING", "https://juancarlosgar-mp-ecommerce-php.herokuapp.com//verificador_pp.php?status=pending");
-    define("SUCCESS", "http://localhost/mp-ecommerce-php/verificador_pp.php?status=success");
-    define("FAILURE", "http://localhost/mp-ecommerce-php/verificador_pp.php?status=failure");
-    define("PENDING", "http://localhost/mp-ecommerce-php/verificador_pp.php?status=pending");
+    define("SUCCESS", "https://juancarlosgar-mp-ecommerce-php.herokuapp.com/verificador_pp.php?status=success");
+    define("FAILURE", "https://juancarlosgar-mp-ecommerce-php.herokuapp.com/verificador_pp.php?status=failure");
+    define("PENDING", "https://juancarlosgar-mp-ecommerce-php.herokuapp.com/verificador_pp.php?status=pending");
+    // define("SUCCESS", "http://localhost/mp-ecommerce-php/verificador_pp.php?status=success");
+    // define("FAILURE", "http://localhost/mp-ecommerce-php/verificador_pp.php?status=failure");
+    // define("PENDING", "http://localhost/mp-ecommerce-php/verificador_pp.php?status=pending");
     
     // NOTIFICATION
     define("ENDPOINT", "https://baits.mx/clientes/mp-ecommerce-php/endpoint.php");
